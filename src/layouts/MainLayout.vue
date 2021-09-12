03b88541f717ddcd5f77ac9159a85a2bf79222db
4gname <domian844@gmail.com>
2021-09-12 22:18:01 +0700

    init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+
+  </q-layout>
+</template>
+
+<script>
+
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  setup () {
+
+
+    return {
+
+    }
+  }
+})
+</script>
