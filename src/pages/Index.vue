67fd000102093e67e673ea81df9a19708c67fe1d
4gname <domian844@gmail.com>
2021-12-24 20:21:55 +0700

    add

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -64,7 +64,7 @@ export default defineComponent({
     return {
       locale,
       slide: ref(1),
-      update: 0.15,
+      update: 0.6,
       updates
     }
   }
--- src/pages/Wiki.vue
+++ src/pages/Wiki.vue
@@ -7,7 +7,7 @@
       <q-card flat square style="padding-top: 60px; padding-bottom: 60px;">
         <q-splitter v-model="splitterModel" disable>
           <template v-slot:before>
-            <q-tabs v-model="tab" active-color="blue" class="text-teal" vertical>
+            <q-tabs v-model="tab" active-color="blue" class="text-grey-9" vertical>
               <template v-for="mod in wiki.mods" :key="mod">
                 <q-tab :label="mod.mod" :name="mod.mod" />
               </template>
@@ -15,21 +15,51 @@
           </template>
 
           <template v-slot:after>
-            <q-tab-panels v-model="tab" animated swipeable transition-next="jump-up" transition-prev="jump-up" vertical>
+            <q-tab-panels v-model="tab" animated transition-next="jump-up" transition-prev="jump-up" vertical>
               <template v-for="mod in wiki.mods" :key="mod">
                 <q-tab-panel :name="mod.mod">
                   <q-card-section class="text-h4 q-mb-md" style="text-transform: uppercase">{{ mod.mod }}
                   </q-card-section>
-                  <q-card class="row justify-center" flat>
-                    <div v-for="page in mod.pages" :key="page" class="q-ma-xs" style="width: 250px">
-                      <q-item :disable="page.id > 9" clickable @click="page.id <= 9 ? getNews(page) : ''">
-                        <q-card class="q-pa-none q-ma-none bg-none " flat square style="width: 100%; height: 100%">
-                          <q-img :src="page.img" height="205px" width="205px" />
-                          <q-item-label lines="1" style="text-transform: uppercase">{{ page.name }}</q-item-label>
-                        </q-card>
-                      </q-item>
-                    </div>
+
+                  <q-card flat>
+                    <q-tabs v-model="tabsTiers" active-color="blue" style="color: black">
+                      <template v-for="tier in wiki.tiers" :key="tier">
+                        <q-tab :label="tier.label" :name="tier.name" />
+                      </template>
+                    </q-tabs>
+                    <q-separator/>
                   </q-card>
+                  <q-tab-panels v-model="tabsTiers" animated transition-next="jump-up" transition-prev="jump-up">
+                    <template v-for="tier in wiki.tiers" :key="tier">
+                      <q-tab-panel :name="tier.name">
+                        <q-card class="row justify-center" flat>
+                          <template v-for="page in mod.pages" :key="page">
+                            <div v-if="tier.name === page.tier" class="q-ma-xs" style="width: 250px">
+                              <q-item clickable @click="page.id <= 40 ? getNews(page, mod.id) : ''">
+                                <q-card class="q-pa-none q-ma-none bg-none " flat square style="width: 100%; height: 100%">
+                                  <q-img :src="page.img" height="205px" width="205px" />
+                                  <q-item-label lines="1" style="text-transform: uppercase">
+                                    {{ page.name }}
+                                  </q-item-label>
+                                </q-card>
+                              </q-item>
+                            </div>
+                            <div v-else-if="tier.name === 'all'" class="q-ma-xs" style="width: 250px">
+                              <q-item clickable @click="page.id <= 40 ? getNews(page, mod.id) : ''">
+                                <q-card class="q-pa-none q-ma-none bg-none " flat square style="width: 100%; height: 100%">
+                                  <q-img :src="page.img" height="205px" width="205px" />
+                                  <q-item-label lines="1" style="text-transform: uppercase">
+                                    {{ page.name }}
+                                  </q-item-label>
+                                </q-card>
+                              </q-item>
+                            </div>
+                          </template>
+                        </q-card>
+                      </q-tab-panel>
+                    </template>
+                  </q-tab-panels>
+
                 </q-tab-panel>
               </template>
             </q-tab-panels>
@@ -38,25 +68,33 @@
       </q-card>
 
       <q-dialog v-model="dialog" v-close-popup class="q-pa-none" transition-hide="0" transition-show="0">
-        <q-card class="q-pa-lg" style="width: 800px; max-width: 1200px" flat square>
+        <q-card class="q-pa-lg" flat square style="width: 800px; max-width: 1200px">
 
           <q-card-section class="text-h4 items-center text-bold">{{ titlePage }}</q-card-section>
           <q-img :src="imgPage" height="300px" width="300px" />
 
           <q-card-section class="items-center q-pb-none">
             <q-item class="text-h5 text-bold items-center q-py-none">{{ $t('wiki_desc') }}</q-item>
-            <q-item class="items-center q-py-none" style="min-height: 20px">{{ $t('wiki_decs_' + id) }}</q-item>
+            <q-item class="items-center q-py-none" style="min-height: 20px">
+              {{ $t('wiki_decs_' + modID + "_" + id) }}
+            </q-item>
           </q-card-section>
 
           <q-card-section class="items-center q-pb-none">
             <q-item class="text-h5 items-center text-bold q-py-none">{{ $t('wiki_general') }}</q-item>
-            <q-item class="items-center q-py-none" style="min-height: 20px">{{ $t('wiki_general_' + id)}}</q-item>
+            <q-item class="items-center q-py-none" style="min-height: 20px">
+              {{ $t('wiki_general_' + modID + "_" + id) }}
+            </q-item>
           </q-card-section>
 
           <q-card-section v-for="(other, countOther) in otherPage" :key="other" class="items-center q-pb-none">
-            <q-item v-if="other.title !== ''" class="text-h6 text-bold items-center q-py-none">{{$t('wiki_other_title_' + id + '_' + countOther)}}</q-item>
+            <q-item v-if="other.title !== ''" class="text-h6 text-bold items-center q-py-none">
+              {{ $t('wiki_other_title_' + modID + "_" + id + '_' + countOther) }}
+            </q-item>
             <template v-for="(desc, countDesc) in other.desc" :key="desc">
-              <q-item class="items-center q-py-none" style="min-height: 20px">{{$t('wiki_other_desc_' + id + '_' + countOther + '_' + countDesc)}}</q-item>
+              <q-item class="items-center q-py-none" style="min-height: 20px">
+                {{ $t('wiki_other_desc_' + modID + "_" + id + '_' + countOther + '_' + countDesc) }}
+              </q-item>
             </template>
             <template v-for="img in other.img" :key="img">
               <q-card-section v-if="img.img !== null" class="q-pa-none">
@@ -75,7 +113,7 @@
 </template>
 
 <script>
-import {ref} from 'vue'
+import {onMounted, ref, watch} from 'vue'
 import wiki from 'assets/wiki.js'
 
 export default {
@@ -89,10 +127,14 @@ export default {
     const generalPage = ref("");
     const imgPage = ref("");
     const otherPage = ref([]);
+    const modID = ref(0);
     const id = ref(0);
+    let tabsTiers = ref('all');
+    let tab = ref("impact-core");
 
-    const getNews = (page) => {
+    const getNews = (page, mod) => {
       dialog.value = true;
+      modID.value = mod;
       id.value = page.id;
       titlePage.value = page.name;
       generalPage.value = page.general;
@@ -102,9 +144,10 @@ export default {
     };
 
     return {
-      tab: ref("impact-core"),
+      tab,
+      tabsTiers,
       splitterModel: ref(15),
-      wiki, dialog, titlePage, generalPage, descPage, imgPage, otherPage, id,
+      wiki, dialog, titlePage, generalPage, descPage, imgPage, otherPage, id, modID,
       getNews,
     }
   }
